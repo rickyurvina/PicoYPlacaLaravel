9ee2ac782750196614892f8bfb21801ff3c175d6
rurvina208 <63175822+rurvina208@users.noreply.github.com>
2020-09-16 21:47:36 -0500

    Formato Codigo

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,7 +10,7 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
       integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 <body class="fondo">
-<h1 class="titulo">Satck Builders</h1>
+<h1 class="titulo">Stack Builders</h1>
 @yield('content')
 </body>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
--- resources/views/restriction.blade.php
+++ resources/views/restriction.blade.php
@@ -94,7 +94,7 @@
                                     <div class="ln_solid"></div>
                                     <div class="item form-group">
                                         <div class="col-md-6 col-sm-6 offset-md-3">
-                                            <button type="submit" class="btn btn-success">Submit</button>
+                                            <button type="submit" class="btn btn-success">Verificar</button>
                                         </div>
                                     </div>
                                 </form>
